aa1a6bed69ad8a66a6a12cb58f1b84ce1e39147e
lincot <lincot@disroot.org>
2024-05-25 14:28:40 +0300

    test against std sort

--- tests/test.rs
+++ tests/test.rs
@@ -19,22 +19,26 @@ macro_rules! gen_tests {
                 assert_eq!(arr, [8u8.into(), 5u8.into(), 3u8.into(), 0u8.into()]);
 
                 let mut rng = Pcg64Mcg::new(0xcafe_f00d_d15e_a5e5);
-                let mut buf: [$t; BUF_SIZE] = [0u8.into(); BUF_SIZE];
+                let mut buf_vqsort: [$t; BUF_SIZE] = [0u8.into(); BUF_SIZE];
+                let mut buf_stdsort = buf_vqsort.clone();
                 for _ in 0..10 {
-                    let len = rng.gen_range(0..buf.len());
-                    let data = &mut buf[..len];
+                    let len = rng.gen_range(0..BUF_SIZE);
+                    let data_vqsort = &mut buf_vqsort[..len];
+                    let data_stdsort = &mut buf_stdsort[..len];
 
-                    for x in data.iter_mut() {
-                        *x = rng.gen();
-                    }
-                    vqsort_rs::sort(data);
-                    assert!(is_sorted(data));
+                    data_vqsort.iter_mut().for_each(|x| *x = rng.gen());
+                    data_stdsort.copy_from_slice(data_vqsort);
+                    data_stdsort.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+                    vqsort_rs::sort(data_vqsort);
+                    assert_eq!(data_vqsort, data_stdsort);
+                    assert!(is_sorted(data_vqsort));
 
-                    for x in data.iter_mut() {
-                        *x = rng.gen();
-                    }
-                    vqsort_rs::sort_descending(data);
-                    assert!(is_sorted_descending(data));
+                    data_vqsort.iter_mut().for_each(|x| *x = rng.gen());
+                    data_stdsort.copy_from_slice(data_vqsort);
+                    data_stdsort.sort_unstable_by(|a, b| b.partial_cmp(a).unwrap());
+                    vqsort_rs::sort_descending(data_vqsort);
+                    assert_eq!(data_vqsort, data_stdsort);
+                    assert!(is_sorted_descending(data_vqsort));
                 }
             }
         }
